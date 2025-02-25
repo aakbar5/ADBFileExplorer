60f478ad6979c2aed0eac6a078c45c960e946d9a
aakbar5 <16612387+aakbar5@users.noreply.github.com>
2025-02-25 22:06:03 +0000

    Create log file to capture crash

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ echo "ADBFileExplorer: ${APP_HOME}"
 export PYTHONUNBUFFERED=1
 export QT_QPA_PLATFORM=xcb
 source $APP_HOME/.venv/bin/activate
-python $APP_HOME/src/main.py
+python $APP_HOME/src/main.py 2>&1 | tee -a $APP_HOME/output.log
